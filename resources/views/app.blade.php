033286f25a269f43d1c61cb3bea6d0b35cea4357
Levi Zustiak <91484241+lzustiak@users.noreply.github.com>
2023-03-19 23:29:46 -0500

    [feat] remove react blade directive

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,6 @@
     <title>Fours</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, intial-scale=1.0, maximum-scale=1.0" />
-    @viteReactRefresh
     @vite('resources/ts/app.tsx')
     @inertiaHead
   </head>
